24aa969a378fa9940b0751d12716e29c10a519c6
Jake Morelli <jake.morelli98@gmail.com>
2022-04-11 14:09:11 -0500

    class

---
+++ ml_2.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+nyc = pd.read_csv("ave_hi_nyc_jan_1895-2018.csv")
+
+print(nyc.head(3))
+
+print(nyc.Date.values)
+
+print(nyc.Date.values.reshape(-1, 1))  # first argument # rows second # of columns
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    nyc.Date.values.reshape(-1, 1), nyc.Temperature.values, random_state=11
+)
+
+print(X_train.shape)
+print(X_test.shape)
+
+from sklearn.linear_model import LinearRegression
+
+lr = LinearRegression()
+
+lr.fit(X=X_train, y=y_train)
+
+print(lr.coef_)
+print(lr.intercept_)
+
+
+predicted = lr.predict(X_test)
+
+expected = y_test
+
+
+for p, e in zip(predicted[::5], expected[::5]):
+    print(f"predicted: {p:.2f}, expected {e:.2f}")
+
+predict = lambda x: lr.coef_ * x + lr.intercept_
+
+print(predict(2025))
+
+print(predict(1890))
